7d948bf076bfc71e4f3ae7c26c4487013ea69684
MO-ALCAESAR <70541798+MO-ALCAESAR@users.noreply.github.com>
2020-08-31 23:47:16 +0200

    Update site.js

--- site.js
+++ site.js
@@ -50,3 +50,56 @@ document.getElementById("mySidepanel").style.width = "0";
   document.getElementById("myP").style.visibility="hidden";
   document.getElementById("myP1").style.visibility="hidden";
 }
+
+var popunder=new Array()
+popunder[0]="http://www.target.com"
+
+//Specify the width and height of new popunder window (in pixels).
+var width = '700'; 
+var height = '450';
+
+//these are obvious variables. set "yes" or "no".
+var p = 'scrollbars=yes, resizable=yes, toolbar=yes,' + 'menubar=yes, status=yes, location=yes, left=85, top=20, height=' + height + ',width=' + width;
+
+// Load new PopUnder only once per browser session? (0=no, 1=yes)
+// Putting 0 will cause the Popunder to load every time page is loaded
+// Specifying 1 will cause it to load only once per session
+var one_time=0
+
+// That's it! Don't edit the code below unless you're really good. :-P //
+
+function get_cookie(Name) {
+  var search = Name + "="
+  var returnvalue = "";
+  if (document.cookie.length > 0) {
+    offset = document.cookie.indexOf(search)
+    if (offset != -1) { // if the cookie exists
+      offset += search.length
+      //set the index of beginning value
+      end = document.cookie.indexOf(";", offset);
+
+    if (end == -1) // set the index of the end of cookie value
+         end = document.cookie.length;
+         returnvalue = unescape(document.cookie.substring(offset, end))
+      }
+   }
+  return returnvalue;
+}
+
+function loadornot(){
+if (get_cookie('popunder')==''){
+load_pop_power()
+document.cookie="popunder=yes"
+}
+}
+
+function load_pop_power(){
+win2 = window.open(popunder[Math.floor(Math.random()*(popunder.length))], "bw", p)
+win2.blur()
+window.focus()
+}
+
+if (one_time==0)
+load_pop_power()
+else
+loadornot()
