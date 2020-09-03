0a8990305055ca55f377f990f8fe3cf3d7e79d2c
MO-ALCAESAR <70541798+MO-ALCAESAR@users.noreply.github.com>
2020-09-03 17:56:34 +0200

    Update re.js

--- re.js
+++ re.js
@@ -1,1 +1,7 @@
-
+if(window==window.top){
+window.location.replace("/");
+}
+var url = new URL(window.location.href);
+var refDom =  url.hostname;
+//if  (refDom !== "alcaesar-connect.yoo7.com")
+//window.location.replace("https://alcaesar-connect.yoo7.com/");
