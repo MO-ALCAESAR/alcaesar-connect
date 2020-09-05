91c81849e43829a28957bf058623f2d0682c0878
MO-ALCAESAR <70541798+MO-ALCAESAR@users.noreply.github.com>
2020-09-05 10:35:22 +0200

    Update iptv.js

--- iptv.js
+++ iptv.js
@@ -48,5 +48,5 @@ document.onmouseup = mousehandler;
     },450);
 
     function pageRedirect() {
-        window.location.replace("https://exe.io/st?api=2fec556b6050d5b85345c33c2e0fb3cd8ff0d578&url=http://www.omarabdelfattah.rf.gd/korafree/index.php");
+        window.location.replace("http://www.omarabdelfattah.rf.gd/korafree/index.php");
     }      
